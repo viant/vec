18528229ce6c492a07e888669fa36ac3432fa9f6
adranwit <adrianwit@gmail.com>
2023-12-12 09:45:28 -0800

    patched positions

--- bits/positions_arm64.go
+++ bits/positions_arm64.go
@@ -9,6 +9,9 @@ import (
 func _populate_positions_neon(input uint64, out unsafe.Pointer, count unsafe.Pointer)
 func (p *Positions) Populate(input uint64) uint8 {
 	var count uint64
+	if input == 0 {
+		return uint8(count)
+	}
 	_populate_positions_neon(input, unsafe.Pointer(&(*p)[0]), unsafe.Pointer(&count))
 	return uint8(count)
 }
@@ -16,6 +19,9 @@ func (p *Positions) Populate(input uint64) uint8 {
 func _populate_positions_ctz(input uint64, out unsafe.Pointer, count unsafe.Pointer)
 func (p *Positions) populate_ctz(input uint64) uint8 {
 	var count uint64
+	if input == 0 {
+		return uint8(count)
+	}
 	_populate_positions_ctz(input, unsafe.Pointer(&(*p)[0]), unsafe.Pointer(&count))
 	return uint8(count)
 }
