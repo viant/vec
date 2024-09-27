12acfcc34dc836feb8d2203c808d84811fa95a54
vc42 <boston103@hotmail.com>
2024-09-27 15:56:30 -0400

    fixed amd placeholder

--- blas/int32_amd64.go
+++ blas/int32_amd64.go
@@ -5,10 +5,14 @@ import (
 	"unsafe"
 )
 
-//go:noescape
 func (o Int32s) Inc(constant int32) {
 	o.inc(constant) // This is a fallback implementation, not a real one
 }
+func (o Int32s) ScalarMul(constant int32) {
+	o.scalarMul(constant) // This is a fallback implementation, not a real one
+}
+
+//go:noescape
 
 func _add_int32(input1, input2, output unsafe.Pointer, info uint64)
 func (o Int32s) Add(input1, input2 []int32) {
@@ -18,10 +22,6 @@ func (o Int32s) Add(input1, input2 []int32) {
 func _sub_int32(input1, input2, output unsafe.Pointer, info uint64)
 func (o Int32s) Sub(input1, input2 []int32) {
 	_sub_int32(unsafe.Pointer(&input1[0]), unsafe.Pointer(&input2[0]), unsafe.Pointer(&(o)[0]), uint64(len(o)|cpu.Info))
-}
-
-func (o Int32s) ScalarMul(constant int32) {
-	o.scalarMul(constant) // This is a fallback implementation, not a real one
 }
 
 func _mul_int32(input1, input2, output unsafe.Pointer, info uint64)
